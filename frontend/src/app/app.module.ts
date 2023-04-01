80b72710e7b5bcae66eb8c5bdd2d70d2f11ff45a
ZanaGuda <zanag@solution25.com>
2023-04-01 16:05:19 +0200

    blogs-posts-frontend-implementation

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatCardModule } from '@angular/material/card';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogComponent } from './user/blog/blog.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, BlogComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatCardModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
