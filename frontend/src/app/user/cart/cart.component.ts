cb779d8c91e4cb5a1df396117731b43e160b3432
Lorent Sinani <lorent.sinani0@gmail.com>
2023-04-02 17:17:43 +0200

    implemented basic view of cart

--- frontend/src/app/user/cart/cart.component.ts
+++ frontend/src/app/user/cart/cart.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent {
-
+  products = [
+    {
+      name: 'USB Speaker Portable',
+      price: '$100.99',
+      imgPath: '../../../assets/laptop.png',
+    },
+    {
+      name: 'Joystick Pro',
+      price: '$100.99',
+      imgPath: '../../../assets/laptop.png',
+    },
+    {
+      name: 'Smartphone 5s',
+      price: '$100.99',
+      imgPath: '../../../assets/laptop.png',
+    },
+  ];
 }
