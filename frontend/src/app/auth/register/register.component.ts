eb04292ba8c0848115d65e16cfc8ce39d0425b8e
adnit <adnitkamberi10@gmail.com>
2023-04-03 13:19:27 +0200

    added login and signup pages

--- frontend/src/app/auth/register/register.component.ts
+++ frontend/src/app/auth/register/register.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate(300, style({ opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate(300, style({ opacity: 0 }))
+      ])
+    ]),
+      // trigger('slideInFromTop', [
+      //   transition(':enter', [
+      //     style({ transform: 'translateY(-100%)' }),
+      //     animate('0.3s ease-out', style({ transform: 'translateY(0)' }))
+      //   ])
+      // ])
+    ]
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    // Submit logic
+  }
 }
