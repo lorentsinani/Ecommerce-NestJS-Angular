98cf978550fa229947601d38ec098ebd4fb8b761
Andi-A <Andi-A>
2023-04-07 16:27:49 +0200

    Created an all exception filter

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,8 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { appConfig, IAppConfig } from './config/app-config';
 import dotenv from 'dotenv';
+import { AllExceptionsFilter } from './common/filters/all-execptions.filter';
 
 dotenv.config({
   path: `.env`,
@@ -11,7 +12,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config: IAppConfig = app.get<IAppConfig>(appConfig.KEY);
-
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter as any));
   await app.listen(config.port);
 }
 bootstrap();
