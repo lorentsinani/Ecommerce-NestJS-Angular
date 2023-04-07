00508ca6a62f73ee3e0a4d3f0f7a9b9a489b1ae8
Andi-A <Andi-A>
2023-04-07 21:05:57 +0200

    Generated crud operations for employee

--- backend/src/domain/employees/employees.service.ts
+++ backend/src/domain/employees/employees.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Employee } from '../entities/employee.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { IEmployee } from '../../common/interfaces/employee.interface';
+import { UpdateEmployeeDto } from '../../common/dtos/employees/update-employee';
+import { CreateEmployeeDto } from '../../common/dtos/employees/create-employee';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
 
 @Injectable()
-export class EmployeesService {}
+export class EmployeesService {
+  constructor(@InjectRepository(Employee) private usersRepository: Repository<Employee>) {}
+
+  async findOneById(id: number): Promise<Employee> {
+    const user = await this.usersRepository.findOne({ where: { id } });
+    if (!user) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+    return user;
+  }
+
+  async findAll(): Promise<IEmployee[]> {
+    return this.usersRepository.find();
+  }
+
+  async create(createUserDto: CreateEmployeeDto): Promise<IEmployee> {
+    const user = this.usersRepository.create(createUserDto);
+    return this.usersRepository.save(user);
+  }
+
+  async update(id: number, updateUserDto: UpdateEmployeeDto): Promise<UpdateResult> {
+    const result = await this.usersRepository.update(id, updateUserDto);
+    if (result.affected === 0) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+    return result;
+  }
+
+  async delete(id: number): Promise<DeleteResult> {
+    const result = await this.usersRepository.delete(id);
+    if (result.affected === 0) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+    return result;
+  }
+}
