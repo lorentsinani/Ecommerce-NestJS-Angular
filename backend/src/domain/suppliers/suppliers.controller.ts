53b4f2fba214e547bc79a3cfbc455b8486cc25b9
Lorent Sinani <lorent.sinani0@gmail.com>
2023-04-10 16:37:04 +0200

    Updated CRUD cronology for suppliers

--- backend/src/domain/suppliers/suppliers.controller.ts
+++ backend/src/domain/suppliers/suppliers.controller.ts
@@ -11,32 +11,32 @@ import { NullDtoValidationPipe } from '../../common/pipes/null-dto.validation.pi
 export class SuppliersController {
   constructor(private suppliersService: SuppliersService) {}
 
-
+  @Post()
   @UsePipes(new ValidationPipe())
   @UseFilters(new DuplicateKeyExceptionFilter())
-  async createSupplier(@Body() supplierBody: CreateSupplierDto): Promise<ISuppliers> {
-    return this.suppliersService.createSupplier(supplierBody);
+  async create(@Body() supplierBody: CreateSupplierDto): Promise<ISuppliers> {
+    return this.suppliersService.create(supplierBody);
   }
 
   @Get()
-  async findAllSuppliers() {
+  async findAll(): Promise<ISuppliers[]> {
     return this.suppliersService.findAll();
   }
 
   @Get(':id')
-  async findSupplierById(@Param('id', ParseIntPipe) id: number): Promise<ISuppliers> {
-    return this.suppliersService.findSupplierById(id);
+  async findById(@Param('id', ParseIntPipe) id: number): Promise<ISuppliers> {
+    return this.suppliersService.findById(id);
   }
 
   @Patch(':id')
   @UsePipes(new ValidationPipe(), new NullDtoValidationPipe())
   @UseFilters(new DuplicateKeyExceptionFilter())
-  async updateSupplier(@Param('id', ParseIntPipe) id: number, @Body() supplierBody: UpdateSupplierDto): Promise<ISuppliers> {
-    return this.suppliersService.updateSupplier(id, supplierBody);
+  async update(@Param('id', ParseIntPipe) id: number, @Body() supplierBody: UpdateSupplierDto): Promise<ISuppliers> {
+    return this.suppliersService.update(id, supplierBody);
   }
 
   @Delete(':id')
-  async deleteSupplier(@Param('id', ParseIntPipe) id: number): Promise<ISuppliers> {
-    return this.suppliersService.deleteSupplier(id);
+  async delete(@Param('id', ParseIntPipe) id: number): Promise<ISuppliers> {
+    return this.suppliersService.delete(id);
   }
 }
--- backend/src/domain/suppliers/suppliers.service.ts
+++ backend/src/domain/suppliers/suppliers.service.ts
@@ -1,34 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { ISuppliers } from '../../common/interfaces/suppliers.interface';
 import { CreateSupplierDto } from '../../common/dtos/suppliers/create-supplier.dto';
 import { UpdateSupplierDto } from '../../common/dtos/suppliers/update-supplier.dto';
 import { SuppliersRepository } from './suppliers.repository';
 
+const createExceptionMessage = 'Supplier is not created';
+const findExceptionMessage = 'Supplier not found';
 @Injectable()
 export class SuppliersService {
   constructor(private readonly suppliersRepository: SuppliersRepository) {}
 
-  async createSupplier(supplierBody: CreateSupplierDto): Promise<ISuppliers> {
-    return this.suppliersRepository.createSupplier(supplierBody);
+  async create(supplierBody: CreateSupplierDto): Promise<ISuppliers> {
+    const createdSupplier = await this.suppliersRepository.createSupplier(supplierBody);
+
+    if (!createdSupplier) {
+      throw new HttpException(createExceptionMessage, HttpStatus.BAD_REQUEST);
+    }
+
+    return createdSupplier.raw;
   }
 
   async findAll(): Promise<ISuppliers[]> {
-    return this.suppliersRepository.find();
+    return this.suppliersRepository.findAllSupliers();
   }
 
-  async findSupplierById(id: number): Promise<ISuppliers> {
-    return this.suppliersRepository.findSupplierById(id);
+  async findById(id: number): Promise<ISuppliers> {
+    const supplierExist = await this.suppliersRepository.findSupplierById(id);
+
+    if (!supplierExist) {
+      throw new HttpException(findExceptionMessage, HttpStatus.NOT_FOUND);
+    }
+
+    return supplierExist;
   }
 
-  async findOneByEmail(email: string): Promise<ISuppliers> {
-    return this.suppliersRepository.findSupplierByEmail(email);
+  async findByEmail(email: string): Promise<ISuppliers> {
+    const supplierExist = await this.suppliersRepository.findSupplierByEmail(email);
+
+    if (!supplierExist) {
+      throw new HttpException(findExceptionMessage, HttpStatus.NOT_FOUND);
+    }
+
+    return supplierExist;
   }
 
-  async updateSupplier(id: number, supplierBody: UpdateSupplierDto): Promise<ISuppliers> {
-    return this.suppliersRepository.updateSupplier(id, supplierBody);
+  async update(id: number, supplierBody: UpdateSupplierDto): Promise<ISuppliers> {
+    const updatedSupplier = await this.suppliersRepository.updateSupplier(id, supplierBody);
+
+    if (!updatedSupplier.affected) {
+      throw new HttpException(findExceptionMessage, HttpStatus.NOT_FOUND);
+    }
+
+    return updatedSupplier.raw;
   }
 
-  async deleteSupplier(id: number): Promise<ISuppliers> {
-    return this.suppliersRepository.deleteSupplier(id);
+  async delete(id: number): Promise<ISuppliers> {
+    const deletedSupplier = await this.suppliersRepository.deleteSupplier(id);
+
+    if (deletedSupplier.affected) {
+      throw new HttpException(findExceptionMessage, HttpStatus.NOT_FOUND);
+    }
+
+    return deletedSupplier.raw;
   }
 }
