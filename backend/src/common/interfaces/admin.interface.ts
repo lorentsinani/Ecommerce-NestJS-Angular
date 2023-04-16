188243679c99a3f10a8d47a7ae3400bed0b40607
Lorent Sinani <lorent.sinani0@gmail.com>
2023-04-17 01:37:03 +0200

    refactored admin dto, interface, controller repo and service

--- backend/src/common/interfaces/admin.interface.ts
+++ backend/src/common/interfaces/admin.interface.ts
@@ -2,6 +2,7 @@ import { PermissionLevel } from '../constants/enums/permission-level.enum';
 import { IUser } from './user.interface';
 
 export interface IAdmin {
+  user_id: number;
   user: IUser;
   permission_level: PermissionLevel;
 }
--- backend/src/domain/admin/admin.repository.ts
+++ backend/src/domain/admin/admin.repository.ts
@@ -1,7 +1,6 @@
-import { Admin } from './../entities/admin.entity';
 import { Injectable } from '@nestjs/common';
 import { DataSource, DeleteResult, InsertResult, Repository, UpdateResult } from 'typeorm';
-import { IAdmin } from '../../common/interfaces/admin.interface';
+import { Admin } from './../entities/admin.entity';
 import { User } from '../entities/user.entity';
 import { UpdateAdminDto } from '../../common/dtos/admin/update-admin.dto';
 import { CreateAdminDto } from '../../common/dtos/admin/create-admin.dto';
@@ -12,27 +11,27 @@ export class AdminRepository extends Repository<Admin> {
     super(Admin, dataSource.createEntityManager());
   }
 
-  async createAdmin(createAdminDto: CreateAdminDto): Promise<InsertResult> {
-    return await this.createQueryBuilder().insert().into(Admin).values(createAdminDto).returning('*').execute();
+  createAdmin(createAdminDto: CreateAdminDto): Promise<InsertResult> {
+    return this.createQueryBuilder().insert().into(Admin).values(createAdminDto).returning('*').execute();
   }
 
-  async findAllAdmins(): Promise<IAdmin[]> {
+  findAllAdmins(): Promise<Admin[]> {
     return this.find({ relations: ['user'] });
   }
 
-  async findAdminById(id: number): Promise<IAdmin | null> {
+  findAdminById(id: number): Promise<Admin | null> {
     return this.createQueryBuilder('admin').innerJoinAndSelect('admin.user', 'user', 'user.id = :id', { id }).getOne();
   }
 
-  async findAdminByEmail(email: string): Promise<IAdmin | null> {
+  findAdminByEmail(email: string): Promise<Admin | null> {
     return this.createQueryBuilder('admin').innerJoinAndSelect('admin.user', 'user', 'user.email = :email', { email }).getOne();
   }
 
-  async updateAdmin(user_id: number, updateAdminDto: UpdateAdminDto): Promise<UpdateResult> {
+  updateAdmin(user_id: number, updateAdminDto: UpdateAdminDto): Promise<UpdateResult> {
     return this.createQueryBuilder().update(Admin).set(updateAdminDto).where('user_id = :user_id', { user_id }).execute();
   }
 
-  async deleteAdmin(user_id: number): Promise<DeleteResult> {
+  deleteAdmin(user_id: number): Promise<DeleteResult> {
     return this.manager.getRepository(User).createQueryBuilder().delete().from(User).where('id = :id', { id: user_id }).returning('*').execute();
   }
 }
