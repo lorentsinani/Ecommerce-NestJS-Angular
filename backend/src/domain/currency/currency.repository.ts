78b689879e8476d6ddc9b98548f3d1a4af410e4b
Lorent Sinani <lorent.sinani0@gmail.com>
2023-04-17 00:24:48 +0200

    refactored currency dto, controller, repo and service

--- backend/src/domain/currency/currency.repository.ts
+++ backend/src/domain/currency/currency.repository.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { DataSource, DeleteResult, InsertResult, Repository, UpdateResult } from 'typeorm';
-import { ICurrency } from '../../common/interfaces/currency.interface';
 import { Currency } from '../entities/currency.entity';
 import { CreateCurrencyDto } from '../../common/dtos/currency/create-currency.dto';
 import { UpdateCurrencyDto } from '../../common/dtos/currency/update-currency.dto';
@@ -11,27 +10,27 @@ export class CurrencyRepository extends Repository<Currency> {
     super(Currency, dataSource.createEntityManager());
   }
 
-  async createCurrency(createCurrencyDto: CreateCurrencyDto): Promise<InsertResult> {
+  createCurrency(createCurrencyDto: CreateCurrencyDto): Promise<InsertResult> {
     return this.createQueryBuilder().insert().into(Currency).values(createCurrencyDto).returning('*').execute();
   }
 
-  async findAllCurrencies(): Promise<ICurrency[]> {
+  findAllCurrencies(): Promise<Currency[]> {
     return this.find();
   }
 
-  async findCurrencyById(id: number): Promise<ICurrency | null> {
+  findCurrencyById(id: number): Promise<Currency | null> {
     return this.createQueryBuilder('currency').where('currency.id = :id', { id }).getOne();
   }
 
-  async findCurrencyByCode(code: string): Promise<ICurrency | null> {
+  findCurrencyByCode(code: string): Promise<Currency | null> {
     return this.createQueryBuilder('currency').where('currency.code = :code', { code }).getOne();
   }
 
-  async updateCurrency(id: number, updateCurrencyDto: UpdateCurrencyDto): Promise<UpdateResult> {
+  updateCurrency(id: number, updateCurrencyDto: UpdateCurrencyDto): Promise<UpdateResult> {
     return this.createQueryBuilder().update(Currency).set(updateCurrencyDto).where('id = :id', { id }).returning('*').execute();
   }
 
-  async deleteCurrency(id: number): Promise<DeleteResult> {
+  deleteCurrency(id: number): Promise<DeleteResult> {
     return this.createQueryBuilder().delete().from(Currency).where('id = :id', { id }).returning('*').execute();
   }
 }
