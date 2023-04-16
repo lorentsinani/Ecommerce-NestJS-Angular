7061a439ef219878c024a231d542b8f0d5467d82
Lorent Sinani <lorent.sinani0@gmail.com>
2023-04-17 00:25:34 +0200

    refactored category dto, controller, repo and service

--- backend/src/domain/category/category.repository.ts
+++ backend/src/domain/category/category.repository.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { DataSource, DeleteResult, InsertResult, Repository, UpdateResult } from 'typeorm';
-import { ICategory } from '../../common/interfaces/category.interface';
 import { Category } from '../entities/category.entity';
 import { CreateCategoryDto } from '../../common/dtos/category/create-category.dto';
 import { UpdateCategoryDto } from '../../common/dtos/category/update-category.dto';
@@ -11,27 +10,27 @@ export class CategoryRepository extends Repository<Category> {
     super(Category, dataSource.createEntityManager());
   }
 
-  async createCategory(createCategoryDto: CreateCategoryDto): Promise<InsertResult> {
+  createCategory(createCategoryDto: CreateCategoryDto): Promise<InsertResult> {
     return this.createQueryBuilder().insert().into(Category).values(createCategoryDto).returning('*').execute();
   }
 
-  async findAllCategories(): Promise<ICategory[]> {
+  findAllCategories(): Promise<Category[]> {
     return this.find();
   }
 
-  async findCategoryById(id: number): Promise<ICategory | null> {
+  findCategoryById(id: number): Promise<Category | null> {
     return this.createQueryBuilder('category').where('category.id = :id', { id }).getOne();
   }
 
-  async findCategoryByCategoryName(category_name: string): Promise<ICategory | null> {
+  findCategoryByCategoryName(category_name: string): Promise<Category | null> {
     return this.createQueryBuilder('category').where('category.category_name = :category_name', { category_name }).getOne();
   }
 
-  async updateCategory(id: number, updateCategoryDto: UpdateCategoryDto): Promise<UpdateResult> {
+  updateCategory(id: number, updateCategoryDto: UpdateCategoryDto): Promise<UpdateResult> {
     return this.createQueryBuilder().update(Category).set(updateCategoryDto).where('id = :id', { id }).returning('*').execute();
   }
 
-  async deleteCategory(id: number): Promise<DeleteResult> {
+  deleteCategory(id: number): Promise<DeleteResult> {
     return this.createQueryBuilder().delete().from(Category).where('id = :id', { id }).returning('*').execute();
   }
 }
