787a2827ab85dfa57733f2e8fe1142763c803180
Lorent Sinani <lorent.sinani0@gmail.com>
2023-04-17 00:27:05 +0200

    refactored suppliers dto, controller, repo and service

--- backend/src/common/dtos/suppliers/update-supplier.dto.ts
+++ backend/src/common/dtos/suppliers/update-supplier.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, Length, IsOptional } from 'class-validator';
+import { IsString, Length, IsOptional, IsNotEmpty } from 'class-validator';
 import { UserType } from '../../constants/enums/user-type.enum';
 import { IsCustomEmail } from '../../decorators/email-format.decorator';
 
@@ -6,6 +6,7 @@ export class UpdateSupplierDto {
   @IsString()
   @Length(1, 100)
   @IsOptional()
+  @IsNotEmpty()
   company_name: string;
 
   @IsString()
@@ -15,42 +16,50 @@ export class UpdateSupplierDto {
 
   @IsString()
   @IsOptional()
-  contact_title?: UserType;
+  @IsNotEmpty()
+  contact_title: UserType;
 
   @IsString()
   @Length(1, 100)
   @IsOptional()
+  @IsNotEmpty()
   address: string;
 
   @IsString()
   @Length(1, 50)
   @IsOptional()
+  @IsNotEmpty()
   city: string;
 
   @IsString()
   @Length(1, 50)
   @IsOptional()
-  region?: string;
+  @IsNotEmpty()
+  region: string;
 
   @IsString()
   @Length(1, 20)
   @IsOptional()
+  @IsNotEmpty()
   postal_code: string;
 
   @IsString()
   @Length(1, 50)
   @IsOptional()
+  @IsNotEmpty()
   country: string;
 
   @IsString()
   @Length(1, 20)
   @IsOptional()
+  @IsNotEmpty()
   phone_number: string;
 
   @IsString()
   @Length(1, 20)
   @IsOptional()
-  fax_number?: string;
+  @IsNotEmpty()
+  fax_number: string;
 
   @IsCustomEmail()
   @IsOptional()
