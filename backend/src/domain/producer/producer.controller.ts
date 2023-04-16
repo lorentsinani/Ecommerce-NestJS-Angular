1299d9487f1be3b5ad0e1f3fecd04bfda2a06161
Lorent Sinani <lorent.sinani0@gmail.com>
2023-04-17 00:29:39 +0200

    refactored producer controller, repo and service

--- backend/src/domain/producer/producer.controller.ts
+++ backend/src/domain/producer/producer.controller.ts
@@ -2,39 +2,39 @@ import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UseFil
 import { CreateProducerDto } from '../../common/dtos/producer/create-producer.dto';
 import { ProducerService } from './producer.service';
 import { UpdateProducerDto } from '../../common/dtos/producer/update-producer.dto';
-import { DuplicateKeyExceptionFilter } from '../../common/filters/duplicate-key-exception.filter';
 import { NullDtoValidationPipe } from '../../common/pipes/null-dto.validation.pipe';
+import { QueryExceptionFilter } from '../../common/filters/query.exception.filter';
+import { Producer } from '../entities/producer.entity';
 
 @Controller('producer')
 @UsePipes(new ValidationPipe())
+@UseFilters(new QueryExceptionFilter('User'))
 export class ProducerController {
   constructor(private producerService: ProducerService) {}
 
   @Post()
-  @UseFilters(new DuplicateKeyExceptionFilter('User'))
-  create(@Body() createProducerDto: CreateProducerDto) {
+  create(@Body() createProducerDto: CreateProducerDto): Promise<Producer> {
     return this.producerService.create(createProducerDto);
   }
 
   @Get()
-  findAll() {
+  findAll(): Promise<Producer[]> {
     return this.producerService.findAll();
   }
 
   @Get(':id')
-  findById(@Param('id', ParseIntPipe) id: number) {
+  findById(@Param('id', ParseIntPipe) id: number): Promise<Producer> {
     return this.producerService.findById(id);
   }
 
   @Patch(':id')
-  @UseFilters(new DuplicateKeyExceptionFilter('User'))
   @UsePipes(new NullDtoValidationPipe())
-  update(@Param('id') id: number, @Body() updateProducerDto: UpdateProducerDto) {
+  update(@Param('id') id: number, @Body() updateProducerDto: UpdateProducerDto): Promise<Producer> {
     return this.producerService.update(id, updateProducerDto);
   }
 
   @Delete(':id')
-  delete(@Param('id') id: number) {
+  delete(@Param('id') id: number): Promise<Producer> {
     return this.producerService.delete(id);
   }
 }
