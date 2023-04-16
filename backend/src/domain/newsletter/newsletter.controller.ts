27a6f7002dc468eec30a887edb50871455772c6c
Lorent Sinani <lorent.sinani0@gmail.com>
2023-04-17 00:28:02 +0200

    refactored newsletter controller, repo and service

--- backend/src/domain/newsletter/newsletter.controller.ts
+++ backend/src/domain/newsletter/newsletter.controller.ts
@@ -1,46 +1,45 @@
 import { Body, Controller, Delete, Get, Param, Query, ParseIntPipe, Patch, Post, UseFilters, UsePipes, ValidationPipe } from '@nestjs/common';
-import { DuplicateKeyExceptionFilter } from '../../common/filters/duplicate-key-exception.filter';
 import { NewsletterService } from './newsletter.service';
-import { INewsletter } from '../../common/interfaces/newsletter.interface';
 import { UpdateNewsletterDto } from '../../common/dtos/newsletter/update-newsletter.dto';
 import { NullDtoValidationPipe } from '../../common/pipes/null-dto.validation.pipe';
 import { CreateNewsletterDto } from '../../common/dtos/newsletter/create-newsletter.dto';
+import { QueryExceptionFilter } from '../../common/filters/query.exception.filter';
+import { Newsletter } from '../entities/newsletter.entity';
 
 @Controller('newsletter')
+@UseFilters(new QueryExceptionFilter('Newsletter'))
 export class NewsletterController {
   constructor(private newsletterService: NewsletterService) {}
 
   @Post()
   @UsePipes(new ValidationPipe())
-  @UseFilters(new DuplicateKeyExceptionFilter('Newsletter'))
-  async create(@Body() createNewsletterDto: CreateNewsletterDto): Promise<INewsletter> {
+  create(@Body() createNewsletterDto: CreateNewsletterDto): Promise<Newsletter> {
     return this.newsletterService.create(createNewsletterDto);
   }
 
   @Get()
-  async findAll(): Promise<INewsletter[]> {
+  findAll(): Promise<Newsletter[]> {
     return this.newsletterService.findAll();
   }
 
   @Get(':id')
-  async findById(@Param('id', ParseIntPipe) id: number): Promise<INewsletter> {
+  findById(@Param('id', ParseIntPipe) id: number): Promise<Newsletter> {
     return this.newsletterService.findById(id);
   }
 
   @Get()
-  async findByEmail(@Query('email') email: string): Promise<INewsletter> {
+  findByEmail(@Query('email') email: string): Promise<Newsletter> {
     return this.newsletterService.findByEmail(email);
   }
 
   @Patch(':id')
   @UsePipes(new NullDtoValidationPipe())
-  @UseFilters(new DuplicateKeyExceptionFilter('Newsletter'))
-  async update(@Param('id', ParseIntPipe) id: number, @Body() createNewsletterDto: UpdateNewsletterDto): Promise<INewsletter> {
+  update(@Param('id', ParseIntPipe) id: number, @Body() createNewsletterDto: UpdateNewsletterDto): Promise<Newsletter> {
     return this.newsletterService.update(id, createNewsletterDto);
   }
 
   @Delete(':id')
-  async delete(@Param('id', ParseIntPipe) id: number): Promise<INewsletter> {
+  delete(@Param('id', ParseIntPipe) id: number): Promise<Newsletter> {
     return this.newsletterService.delete(id);
   }
 }
