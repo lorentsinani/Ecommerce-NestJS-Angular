9e0e8a88240dcb22c723b56f21a2802199a8cc7f
adnit <adnitkamberi10@gmail.com>
2023-04-16 12:59:50 +0200

    initial commit

--- backend/src/domain/mailer/mailer.service.ts
+++ backend/src/domain/mailer/mailer.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { createTransport, Transporter } from 'nodemailer';
+import * as handlebars from 'handlebars';
+import * as fs from 'fs';
 
 @Injectable()
-export class MailerService {}
+export class MailerService {
+  private readonly transporter: Transporter;
+
+  constructor(@Inject('MAILER_PROVIDER') transporter: Transporter) {
+    this.transporter = transporter;
+  }
+
+  async sendMail(to: string, subject: string, template: string, context: Record<string, any>): Promise<void> {
+    const html = await this.renderTemplate(template, context);
+
+    await this.transporter.sendMail({
+      to,
+      subject,
+      html
+    });
+  }
+
+  private async renderTemplate(template: string, context: Record<string, any>): Promise<string> {
+    const templateContent = await fs.promises.readFile(`src/domain/mailer/templates/${template}.hbs`, 'utf-8');
+    const compiledTemplate = handlebars.compile(templateContent);
+    return compiledTemplate(context);
+  }
+}
