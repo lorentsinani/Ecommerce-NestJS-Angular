6d0625fe6939f825dfa76139d61bb72a8d688abd
Aridon Krasniqi <aridonk16@outlook.com>
2023-05-08 22:12:14 +0200

    Created the permissions components

---
+++ frontend/src/app/admin/admin-panel/admin-dashboard/users-access-control/permissions/permission.routing.ts
@@ -0,0 +1,9 @@
+import { Route } from '@angular/router';
+import { PermissionsComponent } from './permissions.component';
+
+export const PermissionsRoute: Route[] = [
+  // {
+  //   path: '',
+  //   component: PermissionsComponent
+  // }
+];
