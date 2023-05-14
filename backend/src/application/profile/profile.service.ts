00f278ba7ecc227997c6fbe6b5e0ed652d209a98
Aridon Krasniqi <aridonk16@outlook.com>
2023-05-14 23:12:29 +0200

    Added updateUserPassword method to profile service and controller, so user can change the password when he is authenticated

--- backend/src/application/profile/profile.service.ts
+++ backend/src/application/profile/profile.service.ts
@@ -1,7 +1,9 @@
 import { UpdateUserDto } from './../../common/dtos/users/update-user.dto';
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { UsersService } from '../../domain/users/users.service';
 import { User } from '../../domain/entities/user.entity';
+import { ChangePasswordDto } from '../../common/dtos/profile/change-password.dto';
+import { PasswordUtil } from '../../common/utils/password.util';
 
 @Injectable()
 export class ProfileService {
@@ -13,5 +15,26 @@ export class ProfileService {
 
   updateUserDetails(userId: number, updateUserDto: UpdateUserDto): Promise<User> {
     return this.usersService.update(userId, updateUserDto);
+  }
+
+  async updateUserPassword(userId: number, changePasswordDto: ChangePasswordDto): Promise<User> {
+    const user = await this.usersService.findById(userId);
+
+    if (!user) {
+      throw new HttpException(`User does not exist`, HttpStatus.NOT_FOUND);
+    }
+
+    const { currentPassword, newPassword } = changePasswordDto;
+
+    const passwordMatch = await PasswordUtil.comparePassword(currentPassword, newPassword);
+
+    if (!passwordMatch) {
+      throw new HttpException(
+        `Unable to change password. The old password entered is incorrect. Please try again with the correct old password.`,
+        HttpStatus.UNAUTHORIZED
+      );
+    }
+
+    return this.usersService.update(userId, { password: newPassword } as UpdateUserDto);
   }
 }
